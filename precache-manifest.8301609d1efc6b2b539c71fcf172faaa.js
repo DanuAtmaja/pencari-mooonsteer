aa107fa9c78354fb0c4f4e3c254869d5ce226e54
Danu <danuatmaja@gmail.com>
2020-04-01 18:49:22 +0800

    Updates

---
+++ precache-manifest.8301609d1efc6b2b539c71fcf172faaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9651167bc421eaaa0826eacee9ad6e4e",
+    "url": "/pencari-mooonsteer/index.html"
+  },
+  {
+    "revision": "811b6a7bb259bf28d3c6",
+    "url": "/pencari-mooonsteer/static/css/main.c1b5e86d.chunk.css"
+  },
+  {
+    "revision": "04f5195ff15feb5913c3",
+    "url": "/pencari-mooonsteer/static/js/2.88b06007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pencari-mooonsteer/static/js/2.88b06007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811b6a7bb259bf28d3c6",
+    "url": "/pencari-mooonsteer/static/js/main.d6f796ab.chunk.js"
+  },
+  {
+    "revision": "8b6aad14c0a93c9f38a2",
+    "url": "/pencari-mooonsteer/static/js/runtime-main.f59317d9.js"
+  }
+]);
